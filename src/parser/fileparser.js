af625dfc83b2cc772ba9abfa74e2ff80695c2dfa
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-06-09 16:11:15 -0700

    add input file parsing

---
+++ src/parser/fileparser.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+/**
+ * Parses input file in comma separated or newline separated format.
+ */
+function parseInputFile(file) {
+    let lines = fs.readFileSync(file).toString();
+    lines = lines.split(/\n|[,]/);
+    let ret = [];
+
+    for (const line of lines) {
+        ret.push(line);
+    }
+    return ret;
+}
