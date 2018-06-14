7458789de41663bec5ee2153c6ae096efc0c7d8f
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-06-13 20:31:12 -0700

    add small test for query sending

--- test/testrunner.js
+++ test/testrunner.js
@@ -1,6 +1,7 @@
 /* eslint-disable indent */
 const expect = require("chai").expect;
 const parseInputFile = require("../src/parser/fileparser.js");
+const queryDB = require("../src/querydb.js");
 
 describe("Input file parsing", function () {
 		it("Should parse two line file separated by newline", () => {
@@ -18,4 +19,20 @@ describe("Input file parsing", function () {
 				const res = parseInputFile.parseInputFile(filepath);
 				expect(res).to.have.length(expectLines);
 		});
+
+		it("Should return two valid results from sending query", async () => {
+				const filepath = "./test/data/frog.double.nl.txt";
+				const res = parseInputFile.parseInputFile(filepath);
+				let response;
+
+				try {
+						response = await queryDB.sendQuery(res);
+				} catch (err) {
+						response = err;
+				} finally {
+						expect(response["worldNames"]).to.have.length(2);
+						expect(response["errorNames"]).to.have.length(0);
+						expect(response["worldNames"]).to.deep.equal(["Acanthixalus sonjae", "Afrixalus dorsalis"]);
+				}
+		})
 });
