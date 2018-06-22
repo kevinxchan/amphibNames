dd788959ef87ba78a7a5bbdb423b1aaa13c0cab6
Kevin Chan <kevchan1@alumni.ubc.ca>
2018-06-22 09:40:33 -0700

    stubs for pages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
+/* eslint-disable indent */
 const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-    // res.render("index", { title: "amphibNames" });
-    res.render("form", { title: "amphibNames" });
+router.get("/", function (req, res) {
+    res.render("index", { title: "amphibNames" });
+    // res.render("form", { title: "amphibNames" });
+});
+
+router.post("/", (req, res) => {
+		res.render("form", { title: "post" });
 });
 
 module.exports = router;
